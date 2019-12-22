c2f6c2b4e38eaacd5c8ff1724246dea95267c6fc
Jiwoo Choi <jchoi136@gmail.com>
2019-12-21 16:07:40 -0800

    solves problem 5

--- js0/5.js
+++ js0/5.js
@@ -8,7 +8,15 @@
  */
 
 const solution = (a, b, c) => {
-  return 0
+  if ( a >= b && a >= c) {
+    return a
+  }
+  else if ( b > a && b >= c) {
+    return b
+  }
+  else{
+    return c
+  }
 }
 
 module.exports = {
